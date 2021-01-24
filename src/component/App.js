eccf146e4cabbd410d423c24ceb352ae417b8f15
asaribas35 <asaribas3535@gmail.com>
2021-01-24 09:59:01 +0300

    başlangıç taslağı olustu

---
+++ src/component/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="header">
+          <div className="container">
+            <div className="row">
+                
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
