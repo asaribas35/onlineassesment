5eb7dc0993079d2b76364f09f0be45d031e4de65
asaribas35 <asaribas3535@gmail.com>
2021-01-24 16:48:37 +0300

    fonksyon class a cevrildi

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,10 @@ import './App.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
-function App() {
+class App extends React.Component {
+
+    render(){
+
   return (
     <div className="App">
       {/* -----------------------------HEADER------------------------------------------ */}
@@ -15,48 +18,63 @@ function App() {
         </div>
         <div className="container">
           <div className="row">
-            <span className="navbar-brand mb-0 h1 logo">Online Education Assesment</span>
+            <span className="navbar-brand mb-0 h1 logo">Online Education Assesment
+            </span>
+            <div className="nav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                <button type="button" className="btn btn-light ">Products</button>
+                </li>
+                <li class="nav-item">
+                <button type="button" className="btn btn-light ">Help</button>
+                </li>
+                <li class="nav-item">
+                <button type="button" className="btn btn-light buton3">ContactUs</button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <hr></hr>
         <div className="container">
           <div className="row">
-          <span className="navbar-brand mb-0 h1 sText">kwi</span>
-              <SearchBar />
+            <span className="navbar-brand mb-0 h1 sText">kwi</span>
+            <SearchBar />
             <div className="buttons">
-            <button type="button" className="btn btn-danger buton1">Sign Up</button>
+              <button type="button" className="btn btn-danger buton1">Sign Up</button>
               <button type="button" className="btn btn-light buton2">Login</button>
             </div>
-            </div>
           </div>
-          <hr></hr>
         </div>
-        {/* -----------------------------HEADER------------------------------------------ */}
-        {/* ------------------------------Content------------------------------------*/}
-
-         <div className="container">
-         <p>Last Exams</p>
-         <Exam />
-         <hr></hr>
-         <Exam />
-         <hr></hr>
-         <Exam />
-         <hr></hr>
-         <Exam />
-         <hr></hr>
-         <Exam />
-
-         </div>
-
-        
-
-
-
-
-
-        {/* ------------------------------Content------------------------------------*/}
+        <hr></hr>
       </div>
-  );
+      {/* -----------------------------HEADER------------------------------------------ */}
+      {/* ------------------------------Content------------------------------------*/}
+
+      <div className="container">
+        <p>Last Exams</p>
+        <Exam />
+        <hr></hr>
+        <Exam />
+        <hr></hr>
+        <Exam />
+        <hr></hr>
+        <Exam />
+        <hr></hr>
+        <Exam />
+
+      </div>
+
+
+
+
+
+
+
+      {/* ------------------------------Content------------------------------------*/}
+    </div>
+       );
+    }
 }
 
 export default App;
