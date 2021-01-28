43b97ed20c3099db1d63e925ae60313ea4689810
asaribas35 <asaribas3535@gmail.com>
2021-01-28 21:55:28 +0300

    github a gonder

--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SearchBar from './searchBar';
 import ExamList from './examList';
 import LoginPage from './LoginPage';
+import ExamPage from './ExamPage'
+
 
 import './App.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -10,151 +12,156 @@ import '../../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 class App extends React.Component {
 
   state = {
-      exams: [       
-     {
+    exams: [
+      {
         "id": 1,
-        "lesson":"algorithms",
-        "teacher":"John Doe",
-        "type":"Quiz",
-        "lastTime":"22/02/2021",
+        "lesson": "algorithms",
+        "teacher": "John Doe",
+        "type": "Quiz",
+        "lastTime": "22/02/2021",
         "time": 60
-    },
-    {
+      },
+      {
         "id": 2,
-        "lesson":"Data",
-        "type":"Quiz",
-        "teacher":"Martin Bridge",
-        "lastTime":"20/02/2021",
+        "lesson": "Data",
+        "type": "Quiz",
+        "teacher": "Martin Bridge",
+        "lastTime": "20/02/2021",
         "time": 30
-    },
-    {
+      },
+      {
         "id": 3,
-        "lesson":"AI",
-        "type":"Midterm",
-        "teacher":"Ahmet uslu",
-        "lastTime":"18/02/2021",
+        "lesson": "AI",
+        "type": "Midterm",
+        "teacher": "Ahmet uslu",
+        "lastTime": "18/02/2021",
         "time": 60
-    },
-    {
+      },
+      {
         "id": 4,
-        "lesson":"Math",
-        "type":"Midterm",
-        "teacher":"Aslı Arı",
-        "lastTime":"19/02/2021",
+        "lesson": "Math",
+        "type": "Midterm",
+        "teacher": "Aslı Arı",
+        "lastTime": "19/02/2021",
         "time": 46
-    },
-    {
-      "id": 5,
-      "lesson":"Biology",
-      "type":"Midterm",
-      "teacher":"Serhat Dişli",
-      "lastTime":"19/02/2021",
-      "time": 60
+      },
+      {
+        "id": 5,
+        "lesson": "Biology",
+        "type": "Midterm",
+        "teacher": "Serhat Dişli",
+        "lastTime": "19/02/2021",
+        "time": 60
+      }
+    ],
+
+    search: "",
+
+    loginP: false
+
   }
-  ],
+  searchExam = (event) => {
+    console.log(event.target.value);
 
-     search : "" ,
+    this.setState({ search: event.target.value })
 
-     loginP : false
 
   }
-   searchExam = (event) => {
-     console.log(event.target.value);
 
-      this.setState({search: event.target.value})
-    
-    
-   }
+  loginOC = () => {
 
-   loginOC = () => {
+    this.setState({ loginP: !this.state.loginP })
 
-     this.setState({loginP : !this.state.loginP})
-
-   }
+  }
 
 
-    render(){
+  render() {
 
-      let filteredExams = this.state.exams.filter(
-        (exam) => {
-          return exam.lesson.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
-        }
-      )
+    let filteredExams = this.state.exams.filter(
+      (exam) => {
+        return exam.lesson.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      }
+    )
 
 
 
-  return (
-    <div className="App">
+    return (
       
-      {/* -----------------------------HEADER------------------------------------------ */}
-      <div className="header">
-        <div className="header-top">
 
-        </div>
-        <div className="container">
-          <div className="row">
-            <span className="navbar-brand mb-0 h1 logo">Online Education Assesment
+        <div className="App">
+
+          {/* -----------------------------HEADER------------------------------------------ */}
+          <div className="header">
+            <div className="header-top">
+
+            </div>
+            <div className="container">
+              <div className="row">
+                <span className="navbar-brand mb-0 h1 logo">Online Education Assesment
             </span>
-            <div className="nav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                <button type="button" className="btn btn-light ">Products</button>
-                </li>
-                <li class="nav-item">
-                <button type="button" className="btn btn-light ">Help</button>
-                </li>
-                <li class="nav-item">
-                <button type="button" className="btn btn-light buton3">ContactUs</button>
-                </li>
-              </ul>
+                <div className="nav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <button type="button" className="btn btn-light ">Products</button>
+                    </li>
+                    <li class="nav-item">
+                      <button type="button" className="btn btn-light ">Help</button>
+                    </li>
+                    <li class="nav-item">
+                      <button type="button" className="btn btn-light buton3">ContactUs</button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <hr></hr>
-        <div className="container">
-          <div className="row">
-            <span className="navbar-brand mb-0 h1 sText">kwi</span>
-            <SearchBar 
-              searchExamProp={this.searchExam}
-            />
-            
-            <div className="buttons">
-              <button type="button" className="btn btn-danger buton1">Sign Up</button>
-              <button onClick={this.loginOC}  type="button" className="btn btn-light buton2">Login</button>
-            </div>
-          </div>
-        </div>
-        <hr></hr>
-      </div>
-      {/* -----------------------------HEADER------------------------------------------ */}
-      {/* ------------------------------Content------------------------------------*/}
+            <hr></hr>
+            <div className="container">
+              <div className="row">
+                <span className="navbar-brand mb-0 h1 sText">kwi</span>
+                <SearchBar
+                  searchExamProp={this.searchExam}
+                />
 
-      <div className="container">
-        <p>Last Exams</p>
-        
-        <ExamList 
-            exams ={filteredExams}
-            
-        />
-        
-        
-      </div>
-        {/* ------------------------------Content------------------------------------*/}
-                {/* ------------------------------LoginPage------------------------------------*/}
-                  {
-                      (this.state.loginP) ?
-                        <LoginPage 
-                          loginOC ={this.loginOC}
-                        /> : null
-                  }
-                     
-                {/* ------------------------------LoginPage---------------------------------*/}        
-        <div>
-        
+                <div className="buttons">
+                  <button type="button" className="btn btn-danger buton1">Sign Up</button>
+                  <button onClick={this.loginOC} type="button" className="btn btn-light buton2">Login</button>
+                </div>
+              </div>
+            </div>
+            <hr></hr>
+          </div>
+          {/* -----------------------------HEADER------------------------------------------ */}
+          {/* ------------------------------Content------------------------------------*/}
+
+          <div className="container">
+            <p>Last Exams</p>
+
+            <ExamList
+              exams={filteredExams}
+
+            />
+
+
+          </div>
+          {/* ------------------------------Content------------------------------------*/}
+          {/* ------------------------------LoginPage------------------------------------*/}
+          {
+            (this.state.loginP) ?
+              <LoginPage
+                loginOC={this.loginOC}
+              /> : null
+          }
+
+          {/* ------------------------------LoginPage---------------------------------*/}
+          <div>
+
+          </div>
+          <ExamPage />
         </div>
-    </div>
-       );
-    }
+      
+
+    );
+  }
 }
 
 export default App;
